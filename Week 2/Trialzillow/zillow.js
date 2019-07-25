b8bb9da2c9d7c8211005ac7f163376867789408d
Nehaparveen <neehazulfikar@gmail.com>
2019-07-25 12:31:30 +0530

    completed structure

--- Week 2/Trialzillow/zillow.js
+++ Week 2/Trialzillow/zillow.js
@@ -1,19 +1,22 @@
-function myF(id) {
+function myF(id,iconid) {
   document.getElementById(id).classList.toggle("show");
+  document.getElementById(iconid).classList.toggle("icnrot");
 }
 
+
+
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  // console.log( event )
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+// window.onclick = function(event) {
+//   // console.log( event )
+//   if (!event.target.matches('.dropbtn' ) && !event.target.matches('.icn' )) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
    
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
